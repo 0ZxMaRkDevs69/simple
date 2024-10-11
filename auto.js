470a7ce4506ecff77b9b0f1f3b35c2f1af1fc8de
C3aZ4ar0019 <jc8374356@gmail.com>
2024-10-11 11:31:35 +0800

    Update auto.js

--- auto.js
+++ auto.js
@@ -3,7 +3,7 @@ const path = require('path');
 const login = require('./system/chatbox-fca-remake/index');
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 25696;
+const PORT = process.env.PORT || 3000;
 const { initializeBot, fonts } = require('./system/chat');
 //const { OnChat, font } = require('./system/onChat');
 

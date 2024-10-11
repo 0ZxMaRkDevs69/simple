28506bd2d76d39456de994420008d2fd46d5de76
C3aZ4ar0019 <jc8374356@gmail.com>
2024-10-11 18:59:10 +0800

    Create api.js

---
+++ system/api.js
@@ -0,0 +1,8 @@
+module.exports = {
+  markdevs69: "https://markdevs-last-api-ycci.onrender.com",
+  markdevs69v2: "https://markdevs69v2.onrender.com",
+  kim: "https://hiroshi-rest-api.replit.app",
+  liaspark: "https://liaspark.chatbotcommunity.ltd",
+  deku: "https://joshweb.click",
+  snowflakes: "https://mextral.onrender.com"
+  };

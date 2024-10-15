7b9c80609aa8b9077fa868b455316c559a4b3950
C3aZ4ar0019 <jc8374356@gmail.com>
2024-10-15 21:08:00 +0800

    Update api.js

--- system/api.js
+++ system/api.js
@@ -1,6 +1,6 @@
 module.exports = {
-  markdevs69: "https://markdevs-last-api-ycci.onrender.com",
-  markdevs69v2: "https://markdevs69v2.onrender.com",
+  markdevs69: "https://markdevs-api-j1ql.onrender.com",
+  markdevs69v2: "https://markdevs69v2-679r.onrender.com",
   kim: "https://hiroshi-rest-api.replit.app",
   liaspark: "https://liaspark.chatbotcommunity.ltd",
   deku: "https://joshweb.click",
